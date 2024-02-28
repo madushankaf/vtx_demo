32846f442a25717bde910f2e7d30c380a562440a
madushankaf <madushankafonseka@live.com>
2024-02-28 10:32:34 +0530

    adda book

--- react-single-page-app/src/App.js
+++ react-single-page-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAuthContext } from "@asgardeo/auth-react";
-import React, { useState, useEffect } from "react";
-import { useAuthContext } from "@asgardeo/auth-react";
+
 
 function App() {
   const { state, signIn, signOut } = useAuthContext();
